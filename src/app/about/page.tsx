9309b0735388dd12f3f7ab8b96ce6b3cf4a12d99
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-03-27 09:08:03 -0500

    async and sync metadata covered:

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'About our project',
+  description: 'This is all about our sweet project.',
+}
+
 export default function About(){
   return <h1>About page!</h1>
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,32 @@
-export default function ProductDetails({ 
-  params,
-}: { 
-  params: { productId: string };
-})  {
+import { Metadata } from "next"
+
+type Props = {
+  params: {
+    productId: string;
+  }
+}
+
+//can be async or sync. 
+// async
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const title = await new Promise(resolve => {
+    setTimeout(()=> {
+      resolve(`iPhone ${params.productId}`)
+    }, 100)
+  })
+  return {
+    title: `Product ${title}`
+  }
+}
+
+
+// export const generateMetadata = ({ params }: Props): Metadata => {
+//   return {
+//     title: `Product ${params.productId}`
+//   }
+// }
+
+export default function ProductDetails({ params} : Props)  {
   return (
     <>
       <h1>Product Details dynamic for product ID: {params.productId}</h1>
