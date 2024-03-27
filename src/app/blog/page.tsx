9b278d1ba0ea39ee66216c285caa4ade92235701
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-03-27 11:01:29 -0500

    metadata title object

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  //title: { absolute: "Blog" } Will override the template value in layout at app/layout.tsx
+  title: "Blog metadata"
+}
+
 export default function Blog(){
   return<h1>Blog 1!</h1>
 }
