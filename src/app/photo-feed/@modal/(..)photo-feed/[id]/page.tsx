7fa807c4d8b346f9fecc86cbcad3f5239fcb61fa
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-03-29 18:59:32 -0500

    update for modals broken currently

--- src/app/photo-feed/@modal/(..)photo-feed/[id]/page.tsx
+++ src/app/photo-feed/@modal/(..)photo-feed/[id]/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import wondersImages, { WonderImage } from "../../../wonders";
 import Modal from "@/components/modal";
 
-export default function PhotoPage({
+export default function PhotoModal({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const photo: WonderImage = wondersImages.find((p)=> p.id === id)!;
+  const photo: WonderImage = wondersImages.find((p) => p.id === id)!;
 
   return (
     <Modal>
@@ -16,13 +16,12 @@ export default function PhotoPage({
         src={photo.src}
         className="w-full object-cover aspect-square"
       />
-      <div className="bg-white py-4">
-        <h2 className="text-xl font-semibold">
-          {photo.name}
-        </h2>
+
+      <div className="bg-white p-4">
+        <h2 className="text-xl font-semibold">{photo.name}</h2>
         <h3>{photo.photographer}</h3>
         <h3>{photo.location}</h3>
       </div>
     </Modal>
-  )
+  );
 }
--- src/app/photo-feed/page.tsx
+++ src/app/photo-feed/page.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
-import wonders from "./wonders"
+import wonders from "./wonders";
 import Image from "next/image";
 
-export default function Home(){
+export default function Home() {
   return (
     <main className="container mx-auto">
       <h1 className="text-center text-3xl font-bold my-4">
-        New wonders of the world
+        New Wonders of the World
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-        {wonders.map(({ id, src, name}) =>(
+        {wonders.map(({ id, src, name }) => (
           <Link key={id} href={`/photo-feed/${id}`}>
             <Image
               alt={name}
@@ -20,5 +20,5 @@ export default function Home(){
         ))}
       </div>
     </main>
-  )
+  );
 }
